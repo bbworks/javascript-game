cedff27d1f5e228182a1276032abe59efaad75e4
Bradley Biera <30395710+bbworks@users.noreply.github.com>
2021-01-27 07:31:29 -0500

    Small Changes
    OVERVIEW
    Front-End
     o bugfix for bottom of .playlist-item-selection-list-container begin hidden behind .playlist-item-selection-container
     o feature update to change the background color of .playlist-item-selection-move-dialog-move-items depending on their success or failure of the move/removal
    Back-End
     o moved our fps container from the front-end to the back-end, handled by the Engine class

--- js/utilities/engine.js
+++ js/utilities/engine.js
@@ -1,43 +1,104 @@
 function Engine (fps, update, render) {
 	console.log("Inside \"engine.js\".");
 
-	//Declare private constants and variables
-	var fpsContainer;
 	var isUpdated;
 	var animationFrame;
+	var frameTime = 0;
 	var buffer = 0;
-	var throttleCounter = 0;
-	var deThrottleCounter = 0;
-	var throttleTimer = 10
-	var throttleCap = 30;
+
+	//Throttling information
+	var throttleTimer = 10 //Number of consecutive under-performing frames required to initiate throttle
+	var throttleFps = 30; //The FPS we throttle down to on under-performing systems
+	var throttleCounter = 0; //Used to track number of consecutive under-performing frames
+	var deThrottleCounter = 0; //Used to track number of consecutive well-performing frames
+
+	var fpsContainer; //The div that contains our FPS meter
+
+	//Default measurement for engine performance (togglable on click)
+	var fpsMeasurement = "FPS"; //"FPS" or "MS"
+
+	//How often to render our FPS to the screen
+	var renderTimerDuration = 1000;
+	var renderTimerMeasurement = "ms"; //"frames" or "ms"
+
+	var updateCount = 0; //used to track fpsContainer rendering for "ms"
 
 	this.isRunning = false;
 
-	this.fps = fps;
-	this.throttleFps = this.fps;
+	this.fps = fps; //the desired FPS
+	this.currentFPS = this.fps; //the current FPS we're using (the max currently allowed)
 
 	this.engineLastTimestamp = window.performance.now();
 	this.engineCurrentTimestamp;
-	this.engineDeltaTimestamp;
-	this.engineFPS;
+	this.engineFrameDuration;
+	this.engineFPS = 0;
 
 	this.gameLastTimestamp = window.performance.now();
 	this.gameCurrentTimestamp;
-	this.gameDeltaTimestamp;
-	this.gameFPS;
+	this.gameFrameDuration;
+	this.gameFPS = 0;
 
-	//Create the FPS div module
-	fpsContainer = document.getElementById("fps-container");
-	fpsContainer.innerHTML = `Engine FPS: ${null} | Game FPS: ${null}`;
-	document.body.appendChild(fpsContainer);
+	var renderFps = function() {
+		var measurement = (fpsMeasurement === "FPS" ? "FPS" : "Frame");
+		var value = (fpsMeasurement === "FPS" ? "FPS" : "FrameDuration");
+		var unit = (fpsMeasurement === "MS" ? "ms" : "");
+		fpsContainer.innerHTML = `Engine ${measurement}: ${this["engine"+value]}${unit} | Game ${measurement}: ${this["game"+value]}${unit}`;
+	};
+
+	var handleRenderFps = function() {
+		if (renderTimerMeasurement === "frames") {
+			//return if we hit the specified number of frames/interval
+			return animationFrame % renderTimerDuration === 0;
+		}
+		else if (renderTimerMeasurement === "ms") {
+			//return if we surpass the specified time in ms
+			var isReady = frameTime/renderTimerDuration > updateCount;
+			if (isReady) {
+				updateCount++;
+			}
+			return isReady;
+		}
+	};
+
+	var initFpsContainer = function() {
+		fpsContainer = document.createElement("div");
+		fpsContainer.classList.add("fps-container");
+		fpsContainer.style.position = "fixed";
+		fpsContainer.style.bottom = 0;
+		fpsContainer.style.width = "100%";
+		fpsContainer.style.backgroundColor = "rgb(245,245,245)";
+		fpsContainer.style.textAlign = "center";
+		fpsContainer.style.fontFamily = "monospace";
+		document.body.appendChild(fpsContainer);
+		renderFps.apply(this);
+		fpsContainer.addEventListener("click",
+			function(event) {
+				fpsMeasurement = (fpsMeasurement === "FPS" ? "MS" : "FPS");
+				renderFps.apply(this);
+			}.bind(this)
+		);
+	};
+
+	var calculateFps = function(type) {
+		this[type+"CurrentTimestamp"] = window.performance.now();
+		this[type+"FrameDuration"] = this[type+"CurrentTimestamp"]-this[type+"LastTimestamp"];
+		this[type+"FPS"] = (1000/(this[type+"FrameDuration"])).toFixed(1);
+		this[type+"LastTimestamp"] = this[type+"CurrentTimestamp"];
+		if (type === "engine") {
+			buffer += this.engineFrameDuration;
+			frameTime += this.engineFrameDuration;
+			handleThrottle();
+		}
+		this[type+"FrameDuration"] = this[type+"FrameDuration"].toFixed(1); //Neaten the decimal places (for fpsContainer rendering)
+	};
 
 	var handleThrottle = function() {
-		if (this.engineFPS <= throttleCap) {
+		if (this.engineFPS <= throttleFps) {
 			throttleCounter++;
 			if (throttleCounter === throttleTimer) {
 				deThrottleCounter = 0;
-				console.log(`Throttling game to ${throttleCap} fps due to system slowness.`);
-				this.throttleFps = throttleCap;
+				console.log(`Throttling game to ${throttleFps} fps due to system slowness.`);
+				this.currentFPS = throttleFps;
 			}
 		}
 		else {
@@ -45,28 +106,13 @@ function Engine (fps, update, render) {
 			deThrottleCounter++;
 			if (deThrottleCounter === throttleTimer) {
 				console.log(`Dethrottling game back to ${this.fps}.`);
-				this.throttleFps = this.fps;
+				this.currentFPS = this.fps;
 			}
 		}
 	};
 
-	var calculateFps = function(type) {
-		this[type+"CurrentTimestamp"] = window.performance.now();
-		this[type+"DeltaTimestamp"] = this[type+"CurrentTimestamp"]-this[type+"LastTimestamp"];
-		this[type+"FPS"] = Math.floor(1000/(this[type+"DeltaTimestamp"]));
-		this[type+"LastTimestamp"] = this[type+"CurrentTimestamp"];
-		if (type === "engine") {
-			buffer += this.engineDeltaTimestamp;
-			handleThrottle();
-		}
-	};
-
-	var updateFps = function() {
-	  	fpsContainer.innerHTML = `Engine FPS: ${this.engineFPS} | Game FPS: ${this.gameFPS}`; //Only display updated value every so often
-	};
-
 	var handleGameUpdate = function() {
-		if (buffer < 1000/this.throttleFps) {
+		if (buffer < 1000/this.currentFPS) {
 			return false;
 		}
 		return true;
@@ -98,13 +144,13 @@ function Engine (fps, update, render) {
 		}
 
 		//Visibly update the frames per second on-screen every 50 executions
-		if (animationFrame % 50 === 0) {
-			updateFps.apply(this);
+		if (handleRenderFps()) {
+			renderFps.apply(this);
 		}
 
 		//Run the update as many times as necessary, based on our specified frame rate and on how long its been since we last updated
 		while (handleGameUpdate.apply(this)) {
-			buffer -= 1000/this.throttleFps;
+			buffer -= 1000/this.currentFPS;
 			if (this.isRunning) {
 				update.apply(this);
 			}
@@ -125,6 +171,8 @@ function Engine (fps, update, render) {
 		window.cancelAnimationFrame(animationFrame);
 		this.isRunning = false;
 	};
+
+	initFpsContainer.apply(this);
 }
 
 Engine.prototype.constructor = Engine;
